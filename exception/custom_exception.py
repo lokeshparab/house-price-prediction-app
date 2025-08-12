70670b292eb2d06f0e6607cc106609de179c69ae
Lokesh Parab <66030300+lokeshparab@users.noreply.github.com>
2025-08-12 18:19:20 +0530

    Added config, logger, exception

---
+++ exception/custom_exception.py
@@ -0,0 +1,30 @@
+import sys, traceback
+from logger.custom_logger import CustomLogger
+
+logging = CustomLogger().get_logger(__name__)
+
+class CustomException(Exception):
+    """Custom exception class that captures the stack trace and provides a formatted error message."""
+    def __init__(self, error_message: str,error_details:sys):
+        # super().__init__(error_message)
+        _,_,exc_tb = sys.exc_info()
+        self.file_name = exc_tb.tb_frame.f_code.co_filename
+        self.lineno = exc_tb.tb_lineno
+        self.error_message = str(error_message)
+
+        self.traceback_str = "".join(traceback.format_exception(*error_details.exc_info()))
+
+    def __str__(self):
+        return (
+            f"Error in [{self.file_name}] at line [{self.lineno}]\n"
+            f"Message : {self.error_message}\n"
+            f"Traceback:\n{self.traceback_str}"
+        )
+
+if __name__ == "__main__":
+    try:
+        a = 1/0
+    except Exception as e:
+        app_exc = CustomException(e, sys)
+        logging.error(str(app_exc))
+        raise app_exc
